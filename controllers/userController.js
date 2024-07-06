28cbc7cc7dfafdebd045210bfb8293ae26df77c9
MuhammedshihadTP <shihadmuhammedv@gmail.com>
2024-07-06 10:01:58 +0530

    fix:filename

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 
 const updateProfile = async (req, res) => {
   const { name, username,  bio, interestedCountries } = req.body;
